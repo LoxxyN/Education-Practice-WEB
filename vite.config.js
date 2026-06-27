185daa9d46fe378f23f9e8190386e6affda8295a
loxxyNTepk <emoyko@yandex.ru>
2026-06-27 10:48:50 +0700

    Обновил конфиг поменяв базовый url

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/Education-Practice-WEB-/',
+  base: '/Education-Practice-WEB/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
