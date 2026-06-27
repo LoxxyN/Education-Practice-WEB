1b56682fb5ede5772720dd9a03233b299a7fd39b
loxxyNTepk <emoyko@yandex.ru>
2026-06-27 11:15:32 +0700

    Поменял тип создаваемой истории

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from "@pages/MainPage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'MainPage', component: MainPage },
     { path: '/profile', name: 'UserProfilePage', component: () => import('../pages/UserProfilePage.vue') },
